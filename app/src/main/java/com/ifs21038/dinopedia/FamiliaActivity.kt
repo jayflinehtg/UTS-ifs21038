d1a1752bbf146b260d49385b69447e1b4d84589e
Jayfline Hutagalung <jayfline7@gmail.com>
2024-03-18 21:34:24 +0700

    Update baru

--- app/src/main/java/com/ifs21038/dinopedia/FamiliaActivity.kt
+++ app/src/main/java/com/ifs21038/dinopedia/FamiliaActivity.kt
@@ -1,22 +1,52 @@
 package com.ifs21038.dinopedia
 
+import android.os.Build
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import com.ifs21038.dinopedia.databinding.ActivityFamiliaBinding
 
 class FamiliaActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFamiliaBinding
+    private var familia: Familia? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFamiliaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val familia = intent.getParcelableExtra<Familia>(EXTRA_FAMILIA)
-        familia?.let {
-            // Tampilkan detail dinosaurus di sini
-            // Misalnya, binding.tvNamaFamilia.text = it.nama
+        familia = if (Build.VERSION.SDK_INT >= 33) {
+            intent.getParcelableExtra(EXTRA_FAMILIA,
+                Familia::class.java)
+        } else {
+            @Suppress("DEPRECATION")
+            intent.getParcelableExtra(EXTRA_FAMILIA)
         }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        if (familia != null) {
+            supportActionBar?.title = "Menu ${familia!!.name}"
+            loadData(familia!!)
+        } else {
+            finish()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    private fun loadData(familia: Familia) {
+        binding.familiaImage.setImageResource(familia.icon)
+        binding.tvFamiliaName.text = familia.name
+        binding.tvDetailDescription.text = familia.description
+        binding.StringPeriode.text = familia.periode
+        binding.tvDetailCharacteristic.text = familia.characteristic
+        binding.tvDetailHabitat.text = familia.habitat
+        binding.tvPerilaku.text = familia.perilaku
+
+
     }
 
     companion object {
